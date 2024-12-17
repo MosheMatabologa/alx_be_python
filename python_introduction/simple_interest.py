b5af74b37b6a6da6e54baeb9247ece6690456f8c
root <gideonmatabologa@gmail.com>
2024-12-17 10:11:47 -0800

    interest calculation

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+
+principal = 1000
+rate = 0.05
+time = 3
+
+interest = principal * rate * time
 
 
-P = 1000
-R = 0.05
-T = 3
-
-I = P * R * T
-
-print(f'The simple interest is: {I}')
+print(f"The simple interest is: {interest}")
