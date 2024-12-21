3f1530d163471e86425235bc72630c0a12036ddc
root <gideonmatabologa@gmail.com>
2024-12-21 06:56:16 -0800

    added code and made file executable

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-for i in weather:
-    pass
+
+weather_input = input("What's the weather like today? (sunny/rainy/cold): ")
+
+if weather_input == "sunny":
+	print("Wear a t-shirt and sunglasses.")
+
+elif weather_input == "rainy":
+	print("Don't forget your umbrella and a raincoat.")
+
+elif weather_input == "cold":
+	print("Make sure to wear a warm coat and a scarf.")
+
+else:
+	print("Sorry, I don't have recommendations for this weather.")
+
