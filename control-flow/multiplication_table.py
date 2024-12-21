bc287f31f65b5a9253ebdd9d6d7d3cbef9f21e6f
root <gideonmatabologa@gmail.com>
2024-12-21 12:51:02 -0800

    Multiplication Table

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+number = int(input("Enter a number to see its multiplication table: "))
+X = number
+for Y in range(1, 11):
+    Z = number * Y
+    print(f'{X} * {Y} = {Z}')
