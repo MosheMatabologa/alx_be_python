5a65501e69bd4554eb66b8057abd5b24c913bb6f
root <gideonmatabologa@gmail.com>
2024-12-27 16:33:47 -0800

    final task

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+size = int(input("Enter the size of the pattern: "))
+
+
+if size <= 0:
+    print("Please enter a positive integer.")
+else:
+    
+    row = 0
+
+    
+    while row < size:
+        
+        for _ in range(size):
+            print("*", end="")
+        
+        print()
+      
+        row += 1
