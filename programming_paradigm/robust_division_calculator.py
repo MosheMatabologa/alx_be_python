073bcd1441f2602e71738c764cc29887486b920a
root <gideonmatabologa@gmail.com>
2025-01-15 07:12:58 -0800

    Error Handling

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+numerator = float(input("Enter the numerator: "))
+denominaror = float(input("Enter the denominator: "))
+
+def safe_divide(numerator, denominator):
+    try:
+        return numerator / denominator
+    except ZeroDivisionError:
+        return print("Error: Cannot divide by zero.")
+    except ValueError:
+        if numerator == str or denominator == str:
+            return ValueError
+        print("Error: Please enter numeric values only.")
+    finally:
+        if numerator != 0:
+            return
+        print("Division is done.")
+    
+if __name__ == "__main__":
+        #num1 = float(input("Enter the numerator: "))
+        #num2 = float(input("Enter the denominator: "))
+        result = safe_divide(numerator, denominaror)
+        if result is not None:
+            print(f"The result is: {result}")
